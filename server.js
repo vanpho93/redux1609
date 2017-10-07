30e3ca0501c1afd853880edb1ce733deb2960a9b
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-07 15:42:29 +0700

    server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/tinh/:tenPhepTinh/:soA/:soB', (req, res) => {
+    const { tenPhepTinh, soA, soB } = req.params;
+    const pt = new PhepTinh(soA, soB, tenPhepTinh);
+    res.send(pt.getResult() + '');
+});
+
+app.listen(3000, () => console.log('Server started'));
+
+class PhepTinh {
+    constructor(soA, soB, tenPhepTinh) {
+        this.soA = soA;
+        this.soB = soB;
+        this.tenPhepTinh = tenPhepTinh;
+    }
+
+    getResult() {
+        switch (this.tenPhepTinh) {
+            case 'CONG': return +this.soA + +this.soB;
+            case 'TRU': return this.soA - this.soB;
+            case 'NHAN': return this.soA * this.soB;
+            default: return this.soA / this.soB;
+        }
+    }
+}

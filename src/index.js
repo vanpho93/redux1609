5fcd3914d558a79a65f5d852b324a5d1175fd4ed
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-07 10:16:34 +0700

    code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-const defaultState = { value: 0 };
-
-const reducer = (state = defaultState, action) => {
-    if (action.type === 'ADD') return { value: state.value + 1 };
-    if (action.type === 'SUB') return { value: state.value - 1 };
-    return state;
-}
-
-const store = createStore(reducer);
+import store from './redux/store';
 
 const rootDiv = document.getElementById('root');
 ReactDOM.render(<Provider store={store}><App /></Provider>, rootDiv);
 registerServiceWorker();
-// console.log(store.getState());
-// store.dispatch({ type: 'ADD' });
-// console.log(store.getState());
---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+const defaultState = { value: 0 };
+
+const reducer = (state = defaultState, action) => {
+    if (action.type === 'ADD') return { value: state.value + 1 };
+    if (action.type === 'SUB') return { value: state.value - 1 };
+    return state;
+}
+
+const store = createStore(reducer);
+
+export default store;

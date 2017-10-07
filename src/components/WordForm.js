a6ba58ed8fd84845be0f8cdeaf51dcb9c22b4a01
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-07 13:59:54 +0700

    show and hide form

--- src/components/WordForm.js
+++ src/components/WordForm.js
@@ -5,6 +5,7 @@ class WordForm extends Component {
     constructor(props) {
         super(props);
         this.addWord = this.addWord.bind(this);
+        this.toggleForm = this.toggleForm.bind(this);
     }
 
     addWord() {
@@ -13,31 +14,47 @@ class WordForm extends Component {
         const vn = this.refs.txtVn.value;
         const isMemorized = this.refs.cbMemorized.checked;
         dispatch({ type: 'ADD_WORD', en, vn, isMemorized });
+        dispatch({ type: 'TOGGLE_FORM' });        
         this.refs.txtEn.value = '';
         this.refs.txtVn.value = '';
         this.refs.cbMemorized.checked = false;
     }
 
+    toggleForm() {
+        const { dispatch } = this.props;
+        dispatch({ type: 'TOGGLE_FORM' });        
+    }
+
     render() {
+        const { isShowForm } = this.props;
+        const formStyle = isShowForm ? {} : { display: 'none' };
+        const buttonStyle = isShowForm ? { display: 'none' } : {};
         return (
-            <div className="word-form">
-                <input className="form-control" placeholder="English" ref="txtEn" />
-                <br />
-                <input className="form-control" placeholder="Vietnamese" ref="txtVn" />
-                <br />
-                <label><input type="checkbox" ref="cbMemorized" /> Memorized</label>
-                <br />
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <button className="btn btn-primary" onClick={this.addWord}>
-                        Add word
-                    </button>
-                    <button className="btn btn-warning">
-                        Cancel
-                    </button>
+            <div>
+                <div className="word-form" style={formStyle}>
+                    <input className="form-control" placeholder="English" ref="txtEn" />
+                    <br />
+                    <input className="form-control" placeholder="Vietnamese" ref="txtVn" />
+                    <br />
+                    <label><input type="checkbox" ref="cbMemorized" /> Memorized</label>
+                    <br />
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <button className="btn btn-primary" onClick={this.addWord}>
+                            Add word
+                        </button>
+                        <button className="btn btn-warning" onClick={this.toggleForm}>
+                            Cancel
+                        </button>
+                    </div>
                 </div>
+                <button className="btn btn-warning" style={buttonStyle} onClick={this.toggleForm}>
+                    Show Form
+                </button>
             </div>
         );
     }
 }
 
-export default connect()(WordForm);
+const mapState = state => ({ isShowForm: state.isShowForm });
+
+export default connect(mapState)(WordForm);
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,27 +5,32 @@ const defaultState = {
         { en: 'hello', vn: 'xin chào', isMemorized: false },
         { en: 'good morning', vn: 'chào buổi sáng', isMemorized: true },
         { en: 'afternoon', vn: 'buổi chiều', isMemorized: false },
-    ]
+    ],
+    isShowForm: false
 };
 
 const reducer = (state = defaultState, action) => {
     if (action.type === 'ADD_WORD') {
         const { en, vn, isMemorized } = action;
         const word = { en, vn, isMemorized };
-        return { words: state.words.concat(word) };
+        return { ...state, words: state.words.concat(word) };
     }
     if (action.type === 'REMOVE_WORD') {
         const { en } = action;
-        return { words: state.words.filter(word => word.en !== en) };
+        return { ...state, words: state.words.filter(word => word.en !== en) };
     }
     if (action.type === 'TOGGLE_FORGOT') {
         const { en } = action;
         return { 
+            ...state,
             words: state.words.map(word => {
-                if (word.en != en) return word;
+                if (word.en !== en) return word;
                 return { ...word, isMemorized: !word.isMemorized };
             })
         };
+    }
+    if (action.type === 'TOGGLE_FORM') {
+        return { ...state, isShowForm: !state.isShowForm };
     }
     return state;
 }
